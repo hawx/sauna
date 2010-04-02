8b810d12d66d1b735b7261486a4be5698c2de46d
Joshua Hawxwell <joshhawxwell@googlemail.com>
2010-04-02 16:02:30 +0100

    Should now translate non-ascii characters when creating a slug

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -11,16 +11,36 @@ class String
     marked
   end
   
-  # need to make this a lot more robust, it should truncate the slug if
-  #  it gets too long, +other stuff.
+  #  Need to make this a lot more robust, it should truncate the slug if
+  #  it gets too long, + other stuff. Another time maybe?
   def slugify
-    self = self.clone
-    self.gsub!(/[']+/, '')
-    self.gsub!(/\W+/, ' ')
-    self.strip!
-    self.downcase!
-    self.gsub!(' ', '-')
-    self
+    slug = self.clone
+    
+    # @ref: http://snippets.dzone.com/posts/show/2384
+    accents = { ['á','à','â','ä','ã'] => 'a',
+          		  ['Ã','Ä','Â','À','�?'] => 'A',
+          		  ['é','è','ê','ë'] => 'e',
+          		  ['Ë','É','È','Ê'] => 'E',
+          		  ['í','ì','î','ï'] => 'i',
+          		  ['�?','Î','Ì','�?'] => 'I',
+          		  ['ó','ò','ô','ö','õ'] => 'o',
+          		  ['Õ','Ö','Ô','Ò','Ó'] => 'O',
+          		  ['ú','ù','û','ü'] => 'u',
+          		  ['Ú','Û','Ù','Ü'] => 'U',
+          		  ['ç'] => 'c', ['Ç'] => 'C',
+          		  ['ñ'] => 'n', ['Ñ'] => 'N' }
+		accents.each do |ac,rep|
+		  ac.each do |s|
+			str = str.gsub(s, rep)
+		  end
+		end
+
+    slug.gsub!(/[']+/, '')
+    slug.gsub!(/\W+/, ' ')
+    slug.strip!
+    slug.downcase!
+    slug.gsub!(' ', '-')
+    slug
   end
   
 end
