64aa0ca10cef36b9c616689ca2a1779e95d49526
Joshua Hawxwell <joshhawxwell@googlemail.com>
2010-04-02 17:03:38 +0100

    Bugfixes + modified gitignore

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -15,26 +15,6 @@ class String
   #  it gets too long, + other stuff. Another time maybe?
   def slugify
     slug = self.clone
-    
-    # @ref: http://snippets.dzone.com/posts/show/2384
-    accents = { ['á','à','â','ä','ã'] => 'a',
-          		  ['Ã','Ä','Â','À','�?'] => 'A',
-          		  ['é','è','ê','ë'] => 'e',
-          		  ['Ë','É','È','Ê'] => 'E',
-          		  ['í','ì','î','ï'] => 'i',
-          		  ['�?','Î','Ì','�?'] => 'I',
-          		  ['ó','ò','ô','ö','õ'] => 'o',
-          		  ['Õ','Ö','Ô','Ò','Ó'] => 'O',
-          		  ['ú','ù','û','ü'] => 'u',
-          		  ['Ú','Û','Ù','Ü'] => 'U',
-          		  ['ç'] => 'c', ['Ç'] => 'C',
-          		  ['ñ'] => 'n', ['Ñ'] => 'N' }
-		accents.each do |ac,rep|
-		  ac.each do |s|
-			str = str.gsub(s, rep)
-		  end
-		end
-
     slug.gsub!(/[']+/, '')
     slug.gsub!(/\W+/, ' ')
     slug.strip!
