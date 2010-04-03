106a9bcdbc3977b857842e458a3b59bc74888ee8
Joshua Hawxwell <joshhawxwell@googlemail.com>
2010-04-03 16:09:04 +0100

    Form inline labels now play nicely with autofill

--- public/js/form.js
+++ public/js/form.js
@@ -1,27 +1,29 @@
 $(document).ready(function(){
+
   $(".field.inline label + input").each(function (type) {
-   
-    /*Event.observe(window, 'load', function () {
-      setTimeout(function(){
-        if (!input.value.empty()) {
-          input.previous().addClassName('has-text');
-        }
-      }, 200);
-    });*/
     
-     
+    if($(this).val() != "") {
+      $(this).prev("label").removeClass("focus").addClass("has-text");
+    };
+      
     $(this).focus(function () {
       $(this).prev("label").addClass("focus");
+      if($(this).val() != "") {
+        $(this).prev("label").addClass("has-text");
+      } else {
+        $(this).prev("label").removeClass("has-text");
+      };
     });
-     
+    
     $(this).keypress(function () {
       $(this).prev("label").addClass("has-text").removeClass("focus");
     });
+  
      
     $(this).blur(function () {
       if($(this).val() == "") {
         $(this).prev("label").removeClass("has-text").removeClass("focus");
-      }
+      };
     });
     
   });
